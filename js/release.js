2bd55d971b6ca04604edc64f9d9088cbd70e8062
xintianle <20307110196@fudan.edu.cn>
2022-05-26 01:44:12 +0800

    adding: interfaces function in release.js

--- js/release.js
+++ js/release.js
@@ -4,3 +4,18 @@ import {loadNavigation} from "./navigation.js";
 window.onload = function (){
 
 }
+
+function createAPainting(data){
+    var url = "/release/release.php";
+    Ajax.post(url, data, callback);
+
+    function callback(xhr){
+        var jsontext = xhr.responseText;
+        var o = JSON.parse(jsontext);
+
+        var msg = o.message;
+
+        alert(msg);
+    }
+
+}
