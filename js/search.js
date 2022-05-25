7d01ccd001cefc17009db46077b6371a7569f41f
xintianle <20307110196@fudan.edu.cn>
2022-05-25 23:37:00 +0800

    add: interfaces function in search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,31 @@
 import {Ajax} from "./ajax.js";
 import {loadNavigation} from "./navigation.js";
+import {Param} from "./util";
 
 window.onload = function (){
+
+}
+
+function search(text, orderby, page, pagesize){
+    var url = "/search/search.php";
+    var par = {
+        text: text,
+        orderby: orderby,
+        page: page,
+        pagesize: pagesize
+    }
+    var query = Param.parseQueryString(par);
+
+    Ajax.get(url+query, callback);
+
+    function callback(xhr){
+        var jsontext = xhr.responseText;
+        var o = JSON.parse(jsontext);
+
+        var paintings = o.paintings;
+
+        //分页？？？
+    }
+
 
 }
