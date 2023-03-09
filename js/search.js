c49b938ec855e32d559f48483953e979ad0a9681
xintianle <20307110196@fudan.edu.cn>
2023-03-09 13:26:04 +0800

    debug search, delete trivial

--- js/search.js
+++ js/search.js
@@ -64,24 +64,24 @@ function search(text, orderby, page, pagesize){
         var paintings = o.paintings;
         var nums = paintings.length;
 
-        if (xhr.status === 200){
-            //填充搜索结果
-            var searchResultBoxEle = document.getElementById('searchResultBox');
-            if (nums > 0) searchResultBoxEle.innerHTML = "";
-            for (const painting of paintings) {
-                var model = overviewElementModal;
-                fillPaintingByInformation(model, painting);
-                searchResultBoxEle.innerHTML = searchResultBoxEle.innerHTML + model.innerHTML;
-            }
 
-            //显示搜索信息 第几页
-            document.getElementById("search_pageNo").innerText = page;
-
-            //显示一共几页
-            document.getElementById('totalPages').innerText = totalPages;
-            totalpage_now = totalPages;
-
+        //填充搜索结果
+        var searchResultBoxEle = document.getElementById('searchResultBox');
+        searchResultBoxEle.innerHTML = "";
+        for (const painting of paintings) {
+            var model = overviewElementModal;
+            fillPaintingByInformation(model, painting);
+            searchResultBoxEle.innerHTML = searchResultBoxEle.innerHTML + model.innerHTML;
         }
+
+        //显示搜索信息 第几页
+        document.getElementById("search_pageNo").innerText = page;
+
+        //显示一共几页
+        document.getElementById('totalPages').innerText = totalPages;
+        totalpage_now = totalPages;
+
+
     }
 
 
